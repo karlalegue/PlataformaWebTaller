f44ddf8daba443b8c783bdb9a36f04f7b1f0e838
karlaLegue <karla.legue@alumnos.uaysen.cl>
2026-04-18 16:47:17 -0400

    fix: corregir nombres de componentes en App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,16 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import emprendimientolist from "./components/emprendimientolist";
-import emprendimiento from "./components/emprendimiento";
+import EmprendimientoList from "./components/emprendimientolist";
+import Emprendimiento from "./components/emprendimiento";
 import "./estilos.css";
 
-export default function estilos() {
+export default function App() {
   return (
     <BrowserRouter>
       <div className="app-wrapper">
         <Routes>
-
-          <Route path="/" element={<emprendimientolist />} />
-
-  
-          <Route path="/emprendimientos/:id" element={<emprendimiento />} />
-
-        
-          <Route path="*" element={<emprendimientolist />} />
+          <Route path="/" element={<EmprendimientoList />} />
+          <Route path="/emprendimientos/:id" element={<Emprendimiento />} />
+          <Route path="*" element={<EmprendimientoList />} />
         </Routes>
       </div>
     </BrowserRouter>
